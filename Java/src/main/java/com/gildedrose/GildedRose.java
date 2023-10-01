fa0f661e99e41440de041d22681d98fa3d4faee6
Michel Verbist <michel.verbist@chever.com>
2023-10-01 14:47:48 +0200

    - move GildedRose constants from the test to the main code, because they are useful there too.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,14 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
+
+    /** the maximum value that the {@link Item#quality quality} of an {@code Item} can have */
+    public static final Integer MAX_QUALITY = 50;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -22,7 +30,7 @@ class GildedRose {
      * <li>Once the sell by date has passed, Quality degrades twice as fast
      * <li>The Quality of an item is never negative
      * <li>"Aged Brie" actually increases in Quality the older it gets
-     * <li>The Quality of an item is never more than 50
+     * <li>The Quality of an item is never more than {@link #MAX_QUALITY}
      * <li>"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
      * <li>"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
      * 	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
@@ -31,26 +39,26 @@ class GildedRose {
      */
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals(AGED_BRIE)
+                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals(SULFURAS)) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (items[i].quality < MAX_QUALITY) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < MAX_QUALITY) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < MAX_QUALITY) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
@@ -58,15 +66,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(SULFURAS)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!items[i].name.equals(AGED_BRIE)) {
+                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!items[i].name.equals(SULFURAS)) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
@@ -74,7 +82,7 @@ class GildedRose {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (items[i].quality < MAX_QUALITY) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,13 +3,9 @@ package com.gildedrose;
 import net.jqwik.api.*;
 import net.jqwik.api.arbitraries.IntegerArbitrary;
 
-class GildedRoseTest {
+import static com.gildedrose.GildedRose.*;
 
-    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    public static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
-    public static final Integer MAX_QUALITY = 50;
+class GildedRoseTest {
 
     @Property
     boolean theQualityOfAnItemIsNeverNegative(@ForAll("sortOfItems") Item anItem) {
