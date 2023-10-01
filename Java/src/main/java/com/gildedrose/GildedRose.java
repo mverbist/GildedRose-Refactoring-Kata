1673c30c611052bbe627687acf20dd5474071b9a
Michel Verbist <michel.verbist@chever.com>
2023-10-01 14:17:29 +0200

    - add a series of tests to check the behaviour of ''updateQuality()'' according to the business rules in the requirements.
    There weren't any tests before.
    We add these tests before doing any refactoring, so that we can be sure that we don't break anything with our refactorings.
    
    We had to adapt some tests to limit them to 100 years in the past.
    Because the code contains a bug when the ''sellIn'' field has a negative overflow error.
    We avoid this error by limiting the domain to 100 years.
    
    Also added the requirements as javadoc on the ''updateQuality()'' method. We have an IDE that pops-up javadoc when floating over it, so let's use it.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,6 +7,28 @@ class GildedRose {
         this.items = items;
     }
 
+    /**
+     * Updates the {@link Item#quality quality} and the {@link Item#sellIn sellIn} of {@link Item}s.
+     *
+     * This is called at the ed of each day.
+     *
+     * It updates the {@link #items} in the stock of the {@link GildedRose} Inn.
+     *
+     * It follows these general rules for updating the {@code items}:
+     * <li>it lowers the value of the {@code item.sellIn} by 1, because that denotes the number of days by which we have to sell the item</li>
+     * <li>it lowers the value of the {@code item.quality} by 1, because that denotes how valuable the item is</li>
+     *
+     * For some types of {@code items}, it follows these special rules:
+     * <li>Once the sell by date has passed, Quality degrades twice as fast
+     * <li>The Quality of an item is never negative
+     * <li>"Aged Brie" actually increases in Quality the older it gets
+     * <li>The Quality of an item is never more than 50
+     * <li>"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+     * <li>"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+     * 	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+     * 	Quality drops to 0 after the concert
+     *
+     */
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,187 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import net.jqwik.api.*;
+import net.jqwik.api.arbitraries.IntegerArbitrary;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final Integer MAX_QUALITY = 50;
+
+    @Property
+    boolean theQualityOfAnItemIsNeverNegative(@ForAll("sortOfItems") Item anItem) {
+        GildedRose gildedRose = new GildedRose(new Item[]{anItem});
+        gildedRose.updateQuality();
+        return gildedRose.items[0].quality >= 0;
+    }
+
+    @Property
+    boolean theQualityOfAnItemIsNeverMoreThanMAX_QUALITY(@ForAll("sortOfItems") Item anItem) {
+        GildedRose gildedRose = new GildedRose(new Item[]{anItem});
+        gildedRose.updateQuality();
+        return gildedRose.items[0].quality <= MAX_QUALITY;
+    }
+
+    @Property
+    boolean theSellInDateOfItemsDecreases(@ForAll("sortOfItems") Item anItem) {
+        GildedRose gildedRose = new GildedRose(new Item[]{anItem});
+        int sellInBeforeUpdate = anItem.sellIn;
+        gildedRose.updateQuality();
+        int sellInAfterUpdate = gildedRose.items[0].sellIn;
+        return sellInAfterUpdate <= sellInBeforeUpdate;
+    }
+
+    @Property
+    boolean theQualityOfItemsDecreases(@ForAll("sortOfItems") Item anItem) {
+        GildedRose gildedRose = new GildedRose(new Item[]{anItem});
+        int qualityBeforeUpdate = anItem.quality;
+        gildedRose.updateQuality();
+        int qualityAfterUpdate = gildedRose.items[0].quality;
+        return qualityAfterUpdate <= qualityBeforeUpdate;
+    }
+
+    @Property
+    boolean theQualityOfAnAgedBrieIncreases(@ForAll("agedBries") Item anAgedBrie) {
+        GildedRose gildedRose = new GildedRose(new Item[]{anAgedBrie});
+        int qualityBeforeUpdate = anAgedBrie.quality;
+        gildedRose.updateQuality();
+        int qualityAfterUpdate = gildedRose.items[0].quality;
+        return qualityAfterUpdate >= qualityBeforeUpdate;
+    }
+
+    @Property
+    boolean theQualityOfABackstagePassIncreasesBeforeTheSellDate(@ForAll("backstagePassesBeforeSellDate") Item aBackstagePass) {
+        GildedRose gildedRose = new GildedRose(new Item[]{aBackstagePass});
+        int qualityBeforeUpdate = aBackstagePass.quality;
+        gildedRose.updateQuality();
+        int qualityAfterUpdate = gildedRose.items[0].quality;
+        return qualityAfterUpdate == Math.min(qualityBeforeUpdate + 1, MAX_QUALITY);
+    }
+
+    @Property
+    boolean theQualityOfABackstagePassIncreasesBy2WhenLessThan11DaysBeforeTheSellDate(@ForAll("backstagePassesLessThan11DaysBeforeSellDate") Item aBackstagePass) {
+        GildedRose gildedRose = new GildedRose(new Item[]{aBackstagePass});
+        int qualityBeforeUpdate = aBackstagePass.quality;
+        gildedRose.updateQuality();
+        int qualityAfterUpdate = gildedRose.items[0].quality;
+        return qualityAfterUpdate == Math.min(qualityBeforeUpdate + 2, MAX_QUALITY);
+    }
+
+    @Property
+    boolean theQualityOfABackstagePassIncreasesBy3WhenLessThan6DaysBeforeTheSellDate(@ForAll("backstagePassesLessThan6DaysBeforeSellDate") Item aBackstagePass) {
+        GildedRose gildedRose = new GildedRose(new Item[]{aBackstagePass});
+        int qualityBeforeUpdate = aBackstagePass.quality;
+        gildedRose.updateQuality();
+        int qualityAfterUpdate = gildedRose.items[0].quality;
+        return qualityAfterUpdate == Math.min(qualityBeforeUpdate + 3, MAX_QUALITY);
+    }
+
+    @Property
+    boolean theQualityOfABackstagePassDropsToZeroAfterTheConcert(@ForAll("backstagePassesAfterSellDate") Item aBackstagePass) {
+        GildedRose gildedRose = new GildedRose(new Item[]{aBackstagePass});
+        gildedRose.updateQuality();
+        int qualityAfterUpdate = gildedRose.items[0].quality;
+        return qualityAfterUpdate == 0;
+    }
+
+    @Property
+    boolean theSulfurasNeverHaveToBeSold(@ForAll("sulfuras") Item aSulfura) {
+        GildedRose gildedRose = new GildedRose(new Item[]{aSulfura});
+        int sellInBeforeUpdate = aSulfura.sellIn;
+        gildedRose.updateQuality();
+        int sellInAfterUpdate = gildedRose.items[0].sellIn;
+        return sellInAfterUpdate >= sellInBeforeUpdate;
+    }
+
+    @Property
+    boolean theQualityOfSulfurasNeverChanges(@ForAll("sulfuras") Item aSulfura) {
+        GildedRose gildedRose = new GildedRose(new Item[]{aSulfura});
+        int qualityBeforeUpdate = aSulfura.quality;
+        gildedRose.updateQuality();
+        int qualityAfterUpdate = gildedRose.items[0].quality;
+        return qualityAfterUpdate == qualityBeforeUpdate;
+    }
+
+    /** We limit this to 100 years, because the existing code contains a bug at Negative overflow for {@code sellIn} */
+    @Provide
+    IntegerArbitrary allSortOfSellIns() {
+        return Arbitraries.integers().greaterOrEqual(-36500);
+    }
+
+    @Provide
+    Arbitrary<Integer> pastSellIns() {
+        return allSortOfSellIns().lessOrEqual(0);
+    }
+
+    @Provide
+    Arbitrary<Integer> futureSellIns() {
+        return Arbitraries.integers().greaterOrEqual(1);
+    }
+
+    @Provide
+    Arbitrary<Integer> futureSellInsLessThan11Days() {
+        return Arbitraries.integers().between(6, 10);
+    }
+
+    @Provide
+    Arbitrary<Integer> futureSellInsLessThan6Days() {
+        return Arbitraries.integers().between(1, 5);
+    }
+
+    @Provide
+    Arbitrary<Integer> allSortOfQualities() {
+        return Arbitraries.integers().between(0, MAX_QUALITY);
+    }
+
+    @Provide
+    Arbitrary<Item> sortOfItems() {
+        Arbitrary<String> names = Arbitraries.strings().withCharRange('a', 'z').ofMinLength(3).ofMaxLength(21);
+        return Combinators.combine(names, allSortOfSellIns(), allSortOfQualities())
+            .as((name, sellIn, quality) -> new Item(name, sellIn, quality));
+    }
+
+    @Provide
+    Arbitrary<Item> agedBries() {
+        return Combinators.combine(allSortOfSellIns(), allSortOfQualities())
+            .as((sellIn, quality) -> new Item(AGED_BRIE, sellIn, quality));
+    }
+
+    @Provide
+    Arbitrary<Item> backstagePasses() {
+        return Combinators.combine(allSortOfSellIns(), allSortOfQualities())
+            .as((sellIn, quality) -> new Item(BACKSTAGE_PASSES, sellIn, quality));
+    }
+
+    @Provide
+    Arbitrary<Item> backstagePassesBeforeSellDate() {
+        return Combinators.combine(futureSellIns().filter(sellIn -> sellIn > 10), allSortOfQualities())
+            .as((sellIn, quality) -> new Item(BACKSTAGE_PASSES, sellIn, quality));
+    }
+
+    @Provide
+    Arbitrary<Item> backstagePassesLessThan11DaysBeforeSellDate() {
+        return Combinators.combine(futureSellInsLessThan11Days(), allSortOfQualities())
+            .as((sellIn, quality) -> new Item(BACKSTAGE_PASSES, sellIn, quality));
+    }
+
+    @Provide
+    Arbitrary<Item> backstagePassesLessThan6DaysBeforeSellDate() {
+        return Combinators.combine(futureSellInsLessThan6Days(), allSortOfQualities())
+            .as((sellIn, quality) -> new Item(BACKSTAGE_PASSES, sellIn, quality));
+    }
+
+    @Provide
+    Arbitrary<Item> backstagePassesAfterSellDate() {
+        return Combinators.combine(pastSellIns(), allSortOfQualities())
+            .as((sellIn, quality) -> new Item(BACKSTAGE_PASSES, sellIn, quality));
+    }
+
+    @Provide
+    Arbitrary<Item> sulfuras() {
+        return Combinators.combine(allSortOfSellIns(), allSortOfQualities())
+            .as((sellIn, quality) -> new Item(SULFURAS, sellIn, quality));
     }
 
 }
